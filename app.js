5fb035ee5e36d950f07d99b2f478c2469d7af329
Lewis Reed <71259067+Speedyreedy69@users.noreply.github.com>
2020-10-05 11:18:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,110 +1,64 @@
-$(document).ready(function(){
-var turns = ["#","#","#","#","#","#","+","#"];
-var computerTurn = "";
-var turn = "";
-var gameOn = false;
-var count = 0;
-
-var startTurn = prompt("Choose Your Move", "Type X or O").toUpperCase();
-switch (startTurn) {
-    case "X":
-        computerTurn = "O";
-        turn = "X";
-        $("#message").html("Player " + turn + " gets to start!");
-        break;
-    case "O":
-        computerTurn = "X";
-        turn = "O";
-        $("#message").html("Player " + turn + " gets to start!");
-        break;
-    case null:
-        alert("Sorry. Type X or O");
-        window.location.reload(true);
-    break;
-    default:
-        alert("Sorry. Type X or O");
-        window.location.reload(true);
-        break;
+let clicks = 0
+const cells = document.querySelectorAll(".tic");
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+document.getElementById("gameboard").addEventListener("click", function (event) {
+    clicks = (clicks || 0) + 1;
+});
+function restart(){
+    window.location.reload();
 }
-
-function computersTurn() {
-    var taken = false;
-    while (taken === false && count !== 5) {
-        var computerMove = (Math.random() * 10).toFixed();
-        var move = $("#" + computerMove).text();
-        if (move === "#") {
-            $("#" + computerMove).text(computerTurn);
-            taken = true;
-            turns[computerMove] = computerTurn;
+let xMove = [];
+let oMove = [];
+cells.forEach (cell => {
+    cell.addEventListener("click", (event) => {
+        if ((event.target.innerHTML === ("")) && (clicks % 2 == 0)) {
+            event.target.innerHTML = "x";
+            xMove.push(Number(event.target.id));
+        } else if ((event.target.innerHTML === ("")) && (clicks % 2 == 1)) {
+            event.target.innerHTML = "o";
+            oMove.push(Number(event.target.id));
         }
-    }
-}
 
-function playerTurn (turn, id){
-    var spotTaken = $("#"+id).text();
-    if (spotTaken ==="#"){
-    count++;
-    turns[id] = turn;
-    $("#"+id).text(turn);
-    winCondition(turns,turn);
-    if (gameOn === false){
-        computersTurn();
-        $("#message").html("It's " + turn +"'s turn.");
-        winCondition(turns, computerTurn);
-    }
-    }
-}
-
-function winCondition(trackMoves, currentMove) {
-    if (trackMoves[0] === currentMove && trackMoves[1] === currentMove && trackMoves[2] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[2] === currentMove && trackMoves[4] === currentMove && trackMoves[6] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[0] === currentMove && trackMoves[3] === currentMove && trackMoves[6] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[0] === currentMove && trackMoves[4] === currentMove && trackMoves[8] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[1] === currentMove && trackMoves[4] === currentMove && trackMoves[7] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[2] === currentMove && trackMoves[5] === currentMove && trackMoves[8] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[2] === currentMove && trackMoves[5] === currentMove && trackMoves[8] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[3] === currentMove && trackMoves[4] === currentMove && trackMoves[5] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if (trackMoves[6] === currentMove && trackMoves[7] === currentMove && trackMoves[8] === currentMove) {
-        gameOn = true;
-        reset();
-        alert("Player " + currentMove + " wins!");
-    } else if(!(trackMoves.includes("#"))){
-        gameOn = true;
-        reset();
-        alert("It is a Draw!");
-    }﻿ else {
-        gameOn = false;
-    }
-}
-
-$(".tic").click(function(){
-    var slot = $(this).attr('id');
-    playerTurn(turn,slot);
-});
-
-
-});
+      for (let i = 0; i < winningCombos.length; i++) {
+        let count = 0
+        for (let j = 0; j < winningCombos[i].length; j++) { 
+          if (xMove.includes(winningCombos[i][j])){
+            count++
+            if(count === 3){
+            alert("x win")
+            }
+          }
+        }
+      }
+      for (let i = 0; i < winningCombos.length; i++) {
+        let count = 0
+        for (let j = 0; j < winningCombos[i].length; j++) { 
+          if (oMove.includes(winningCombos[i][j])){
+            count++
+            if(count === 3){
+            alert("o win")
+            }
+          }
+        }
+      }
+    });
+})
+function reset(){
+    turns = ["","","","","","","",""];
+    count = 0;
+    document.querySelector(".tic").innerHTML=("");
+    gameOn = true;
+  }
+  
+document.querySelector("#reset").addEventListener("click", function(){
+    reset();
+  });
